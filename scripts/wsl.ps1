d717212011be05742868b969bb77ea8744a29674
Krusing <johan.krusing@hotmail.com>
2025-10-14 17:06:18 +0200

    feature: add keys properties

--- scripts/wsl.ps1
+++ scripts/wsl.ps1
@@ -7,3 +7,11 @@ Write-Host "Files copied."
 Write-Host "Installing package..."
 wsl bash -c "cd /home/andor/.node-red && npm i andor-automation-node-red-dashboard-2-widgets-ui-kioskinput-1.0.0.tgz"
 Write-Host "Done."
+
+Write-Host "Restart node-red..."
+wsl bash -c "pkill -2 node-red"
+
+Write-Host "Removing .config.nodes.json..."
+wsl bash -c "rm -f /home/andor/.node-red/.config.nodes.json"
+
+wsl bash -c "node-red"
