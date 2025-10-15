1c56489f1ec647690772847de3efc37cb318ee5b
Krusing <johan.krusing@hotmail.com>
2025-10-15 09:56:15 +0200

    add file

---
+++ scripts/wsl_install.ps1
@@ -0,0 +1,9 @@
+Write-Host "Copying .tgz files..."
+$source = Get-Location
+$destination = "\\wsl.localhost\debian\home\andor\.node-red"
+Copy-Item -Path (Join-Path $source "*.tgz") -Destination $destination -Force -Verbose -ErrorAction Stop
+Write-Host "Files copied."
+
+Write-Host "Installing package..."
+wsl bash -c "cd /home/andor/.node-red && npm i andor-automation-node-red-dashboard-2-widgets-ui-kioskinput-1.0.0.tgz"
+Write-Host "Done."
